8ad73aa87e809b363ca59db343d2142887b4dd44
RomanEtulain <romanetulain27@gmail.com>
2021-07-09 11:45:21 -0300

    Primer commit

---
+++ WebApiEmpleados/WebApiEmpleados/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebApiEmpleados.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ WebApiEmpleados/WebApiEmpleados/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiEmpleados.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
